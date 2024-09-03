5275af4353240c1161eb22b8bb65132fa262d0dc
Sarath krishna <sasidharansarathkrishna@gmail.com>
2024-09-03 10:43:58 +0530

    Add files via upload

---
+++ sarath/Basics.js
@@ -0,0 +1,27 @@
+// let x=10;
+// {
+//     let x=20;
+// console.log(x)
+
+// }
+// console.log(x)
+
+// let temp = 45
+
+//     if (temp <= 20 )
+// console.log("cold day")
+//  else if(temp>=20 && temp<=30)
+//     console.log("normal day")
+// else
+// console.log("hot day")
+
+// let fruit  = ['apple', 'banana', 'orange'];
+// console.log(fruit);
+// console.log(fruit[1]);
+// fruit.push('kiwi')
+// console.log(fruit);
+// fruit.pop()
+// console.log(fruit);
+
+//Nested Objects:
+// Array of three people
